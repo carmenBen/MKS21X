5b82c0ba600a0ab4eb3f2fe4663dadd2a4eeb8e6
Carmen Benitez <cbenitz@stuy.edu>
2017-11-20 14:35:45 -0500

    Added super array files

---
+++ 01SuperArray/SuperArray.java
@@ -0,0 +1,138 @@
+public class SuperArray{
+    private String[] data;
+    private int size;
+    
+    public SuperArray(){
+	data = new String[10];
+    }
+
+    public SuperArray(int startingCapacity){
+	data = new String[startingCapacity];
+    }
+
+	public void clear(){
+	for(int i = 0;i < size;i++){
+	    data[i] = null;
+	}
+	size = 0;
+    }
+    
+    public int size(){
+	return size;
+    }
+
+    public boolean isEmpty(){
+	if(size == 0){
+	    return true;
+	}
+	return false;
+    }
+
+    public boolean add(String element){
+	if(data.length == size){
+	    this.resize();
+	}
+	data[size] = element;
+	size++;
+	return true;
+    }
+
+    public String toString(){
+	String total = "[";
+	for(int i = 0;i < size - 1;i++){
+	    total = total + data[i] + ",";
+	}
+	if(size != 0){
+	    total += data[size - 1];
+	}
+	total += "]";
+	return total;
+    }
+
+    public String get(int index){
+	if(index < 0 || index>= size){
+	    throw new IndexOutOfBoundsException();
+	}
+	return data[index];
+    }
+
+    public String set(int index, String element){
+	if(index < 0 || index > size){
+	    throw new IndexOutOfBoundsException();
+	}
+	String old = data[index];
+	data[index] = element;
+	return old;
+    }
+
+    private void resize(){
+	String[] newData = new String[size * 2 + 1];
+	for(int i = 0;i < size;i++){
+	    newData[i] = data[i];
+	}
+	data = newData;
+    }
+
+    public boolean contains(String element){
+	for(int i = 0;i < size;i++){
+	    if(data[i] == element){
+		return true;
+	    }
+	}
+	return false;
+    }
+
+    public int indexOf(String element){
+	for(int i = 0;i < size;i++){
+	    if(data[i] == element){
+		return i;
+	    }
+	}
+	return -1;
+    }
+
+    public int lastIndexOf(String element){
+	for(int i = size - 1;i >= 0;i--){
+	    if(data[i] == element){
+		return i;
+	    }
+	}
+	return -1;
+    }
+
+    public void add(int index, String element){
+	if(!(index < 0) && !(index > size)){
+	    if(data.length == size){
+		this.resize();
+	    }
+	    for(int i = size - 1;i >= index;i--){
+		data[i+1] = data[i];
+	    }
+	    size++;
+	    data[index] = element;
+	}
+	else{
+	    throw new IndexOutOfBoundsException();
+	}
+    }
+  
+    public String remove(int index){
+	if(index < 0 || index > size){
+	    throw new IndexOutOfBoundsException();
+	}
+	String remember = data[index];
+	for(int i = index + 1;i < size;i++){
+	    data[i- 1] = data[i];
+	}
+	size--;
+	return remember;
+    }
+
+    public boolean remove(String element){
+	int index = this.indexOf(element);
+	if(this.remove(index) == element){
+	    return true;
+	}
+	return false;
+    }
+}
---
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -0,0 +1,121 @@
+public class OrderedSuperArray extends SuperArray{
+
+    /*constructor initializes an empty List*/
+    public OrderedSuperArray(){
+	super();
+    }
+
+    public OrderedSuperArray(int capacity){
+	super(capacity);
+    }
+
+    public OrderedSuperArray(String[] ary){
+	super(ary.length);
+	for(int i = 0;i < ary.length;i++){
+	    add(ary[i]);
+	}
+    }
+
+    /*call the proper add.*/
+    public void add(int index, String value){
+	add(value);
+    }
+
+    /*Write this method and any
+      appropriate helper methods.*/
+    public boolean add(String value){
+	//add to the correct spot.
+	//you may still use super.add(index,value)
+
+	int index = this.findIndexOfBinary(value);
+	super.add(index,value);
+	return true;
+    }
+
+    public int indexOf(String value){
+	int index = 0;
+	for(int i = 0;i < this.size();i++){
+	    if(this.get(i).compareTo(value) < 0){
+		index = i+1;
+	    }
+	}
+	return index;
+    }
+
+    public int findIndexOfBinary(String value){
+	int start = 0;
+	int end = this.size();
+	int mid = end / 2;
+	int index = 0;
+	boolean found = false;
+	while(!found){
+	    if(end == 0){
+		return 0;
+	    }
+	    else if(value.compareTo(this.get(size() - 1)) > 0){
+		return size();
+	    }
+	    else if(value.compareTo(this.get(mid)) < 0){
+		end = mid;
+		mid = (start + end) / 2;
+	    }
+	    else if(value.compareTo(this.get(mid)) > 0){
+		start = mid;
+		mid = (start + end) / 2;
+	    }
+	    else{
+		found = true;
+		index = mid;
+	    }
+	}
+	return index;
+    }
+
+    public int indexOfBinary(String value){
+	int start = 0;
+	int end = this.size();
+	int mid = end / 2;
+	int index = 0;
+	boolean found = false;
+	while(!found){
+	    if(!contains(value) || end == 0 || value.compareTo(this.get(size() - 1)) > 0 || (value.compareTo(this.get(0)) < 0)){
+		return -1;
+	    }
+	    else if(value.compareTo(this.get(mid)) < 0){
+		end = mid;
+		mid = (start + end) / 2;
+	    }
+	    else if(value.compareTo(this.get(mid)) > 0){
+		start = mid;
+		mid = (start + end) / 2;
+	    }
+	    else if(value.compareTo(this.get(mid)) == 0){
+		found = true;
+		index = mid;
+	    }
+	    else{
+		return -1;
+	    }
+	}
+	return index;
+    }
+
+    public int lastIndexOf(String element){
+	int indexCheck = indexOfBinary(element);
+	int index = indexCheck;
+	boolean check = false;
+	while(!check){
+	    if(element.compareTo(this.get(index + 1)) == 0){
+		index++;
+	    }
+	    else{
+		check = true;
+	    }
+	}
+	return index;
+    }
+
+    public String set(int index, String element){
+        throw new UnsupportedOperationException();
+    }
+}
