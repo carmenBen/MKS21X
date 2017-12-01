37788ef077fa36ba271eee30f6022e205f71aacd
Carmen <cbenitz@stuy.edu>
2017-12-01 17:28:53 -0500

    Wrote Driver with test cases, created Barcode Directory and empty Barcode file

---
+++ 07Barcode/Driver.java
@@ -0,0 +1,8 @@
+public class Driver{
+  Barcode test = new Barcode(10019);
+  System.out.println(test);
+  System.out.println(test.getZip());
+  System.out.println(test.getBar());
+  System.out.println(test.equals("10019"));
+  System.out.println(test.compareTo("10005"));
+}
