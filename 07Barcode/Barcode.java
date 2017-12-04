7029fd933a6c3626536669be8af3623a1ceb239d
Carmen <cbenitz@stuy.edu>
2017-12-04 18:44:53 -0500

    Wrote toZip function and edited Driver to run test cases.

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,48 +1,75 @@
 public class Barcode implements Comparable<Barcode>{
   private String zip;
 
-    public Barcode(String code){
-	if(code.length() != 5){
+  public Barcode(String code){
+    if(code.length() != 5){
 	    throw new IllegalArgumentException();
-	}
-	for(int i = 0;i < 5;i++){
+    }
+    for(int i = 0;i < 5;i++){
 	    if(!Character.isDigit(code.charAt(i))){
-		throw new IllegalArgumentException();
+        throw new IllegalArgumentException();
 	    }
-	}
-	zip = code;
     }
+    zip = code;
+  }
 
-    public String getZip(){
-	return zip;
-    }
+  public String getZip(){
+    return zip;
+  }
     
-    public static String toCode(String zip){
-	if(zip.length() != 5){
+  public static String toCode(String zip){
+    if(zip.length() != 5){
 	    throw new IllegalArgumentException();
-	}
-	for(int i = 0;i < 5;i++){
-	    if(!Character.isDigit(zip.charAt(i))){
-		throw new IllegalArgumentException();
-	    }
-	}
-	String[] code = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
-	String total = "|";
-	for(int i = 0; i < 5;i++){
-	    total += code[Character.getNumericValue(zip.charAt(i))];
-	}
-	total += code[Barcode.checkSum(zip)] + "|";
-	return total;
     }
+    for(int i = 0;i < 5;i++){
+	    if(!Character.isDigit(zip.charAt(i))){
+        throw new IllegalArgumentException();
+	    }
+    }
+    String[] code = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    String total = "|";
+    for(int i = 0; i < 5;i++){
+	    total += code[Character.getNumericValue(zip.charAt(i))];
+    }
+    total += code[Barcode.checkSum(zip)] + "|";
+    return total;
+  }
+
+  public static String toZip(String code){
+    if(code.length() != 32 || code.charAt(0) != '|' || code.charAt(31) != '|'){
+      throw new IllegalArgumentException();
+    }
+    String total = "";
+    String current = "";
+    boolean found = false;
+    String[] bars = {"||:::",":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::"};
+    for(int j = 1;j < 31;j = j+5){
+      current = code.substring(j,j+5);
+      for(int i = 0;i < 10;i++){
+        if(current.equals(bars[i])){
+          total += i;
+          found = true;
+        }
+      }
+      if(!found){
+        throw new IllegalArgumentException();
+      }
+      found = false;
+    }
+    if(Character.getNumericValue(total.charAt(5)) != Barcode.checkSum(total.substring(0,5))){
+      throw new IllegalArgumentException();
+    }
+    return total.substring(0,5);
+  }
     
 
-    public String getCode(){
-	return Barcode.toCode(zip);
-    }
+  public String getCode(){
+    return Barcode.toCode(zip);
+  }
 
-    public String toString(){
-	return getCode() + "   (" + getZip() + ")";
-    }
+  public String toString(){
+    return getCode() + "   (" + getZip() + ")";
+  }
 
   public int compareTo(Barcode other){
     return getZip().compareTo(other.getZip());
--- 07Barcode/Driver.java
+++ 07Barcode/Driver.java
@@ -1,11 +1,11 @@
 public class Driver{
   public static void main(String[] args){
     Barcode test = new Barcode("10019");
-    Barcode test2 = new Barcode("1001");
     System.out.println(test);
     System.out.println(test.getZip());
     System.out.println(test.getCode());
     System.out.println(test.equals(new Barcode("10019")));
     System.out.println(test.compareTo(new Barcode("10005")));
+    System.out.println(Barcode.toZip("|:::||||:::||::::::|||:|:::::|||"));
   }
 }
